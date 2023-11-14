c32be7ad892ae240907ce60ea1f7864298436806
Allan Waiguru <123650511+AllanWaiguru@users.noreply.github.com>
2023-11-14 22:51:59 +0300

    StudentDetails.java

---
+++ StudentDetails.java
@@ -0,0 +1,16 @@
+class StudentDetails{
+public static void main(String[]args){
+
+System.out.println("COURSE:Diploma In Information Technology");
+
+System.out.println("NAME:Allan Waiguru");
+
+System.out.println("REG NO:22/07996");
+
+System.out.println("DOB:01/01/2000");
+
+System.out.println("VISION:Programmer");
+
+System.out.println("FAVOURITE PROGRAMMING LANGUAGE:Java");
+}
+}
